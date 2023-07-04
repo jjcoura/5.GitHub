06c73ce68fdb10e01fee9a23c64f2d5359c5d5b7
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-04 20:48:14 -0300

    a

--- HTML_JS_CSS/aula12/ex014/script.js
+++ HTML_JS_CSS/aula12/ex014/script.js
@@ -1,8 +1,8 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('foto')
+    var msg  = window.document.getElementById('msg')
+    var img  = window.document.getElementById('imagem')
     var data = new Date()
-    var hora =  data.getHours()
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
