935fbf3ec21f59ea07b0c4384bba828cb6fda126
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-04 20:27:22 -0300

    a

--- HTML_JS_CSS/aula12/ex014/script.js
+++ HTML_JS_CSS/aula12/ex014/script.js
@@ -2,13 +2,19 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('foto')
     var data = new Date()
-    var hora = data.getHours()
+    var hora =  data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {// BOM DIA!
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
         img.src = 'manha-redondo.png'
-    } else if (hora >= 12 && hora <= 18) {// BOA TARDE!
-        img.src = 'por-sol-redondo'
-    } else {//BOA NOITE
-        img.src = 'noite-redondo'
+        document.body.style.background = '#f6e5c2'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'por-sol-redondo.png'
+        document.body.style.background = '#a51502'
+    } else {
+        //BOA NOITE
+        img.src = 'noite-redondo.png'
+        document.body.style.background = '#201c1c'
     }
 }
