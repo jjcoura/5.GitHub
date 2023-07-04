cfa7134c07139e97d215b905cb9f6254126c1838
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-04 19:47:42 -0300

    a

--- HTML_JS_CSS/aula12/ex014/script.js
+++ HTML_JS_CSS/aula12/ex014/script.js
@@ -1,1 +1,7 @@
-alert('Olá')
+function carregar () {
+    var msg  = window.document.getElementById('msg')
+    var img  = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+}
