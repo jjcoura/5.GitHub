4334e15a5ced1eb80aecc2a76bca9c065fed4ba4
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-05 19:58:26 -0300

    a

--- HTML_JS_CSS/proj001/script.js
+++ HTML_JS_CSS/proj001/script.js
@@ -5,8 +5,14 @@ function carregar() {
     if (bicho === 'cachorro' || bicho === 'cao' || bicho === 'toto' || bicho === 'totó' || bicho === 'cão') {
         // Cachorro
         img.src = 'ft-cao.png';
-    } else {
+        document.body.style.backgroundColor = '#c2734a';
+    } else if (bicho === 'gato' || bicho === 'bichano' || bicho === 'cat' || bicho === 'gata') {
+        // Gato
+        
+    }
+    else {
         // BICINHO NÃO ENCONTRADO!!!
         img.src = 'imagem-nao-encontrada.png';
+        document.body.style.backgroundColor = '#f5f5f5';
     }
 }
