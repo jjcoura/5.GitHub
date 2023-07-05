075225df8d6a4e5725d72cfba6c5c7e749702014
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-05 19:18:10 -0300

    a

---
+++ HTML_JS_CSS/proj001/script.js
@@ -0,0 +1,11 @@
+function carregar() {
+    var bicho = ''
+    var img = document.createElement('img')    
+    img.setAttribute('id', 'ft')
+        if ( bicho = 'cao') {
+            // Cachorro
+            img.src = 'ft-cao.png'
+        } else {
+            // BICINHO NÂO ENCONTRADO!!!
+        }
+}
