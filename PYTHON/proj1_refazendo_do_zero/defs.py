cdf006364a20e209d6494a591e559b97dbcf3d00
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-10 17:58:36 -0300

    a

--- PYTHON/proj1_refazendo_do_zero/defs.py
+++ PYTHON/proj1_refazendo_do_zero/defs.py
@@ -1,18 +1,3 @@
-"""A função cadastro precisa conter:
-# A função validar()
-nome = validar.nome()
-login = validar.login()
-# Uma função para ler os logins
-lerLogins = open('logins.txt', 'r')
-um "FOR" para correr logins.readlines()
-Se login for igual a valores[1].split() - cria uma lista
-limpaTerminal() - limpa o Terminal
-    senha    = validar.senha()
-    email    = validar.email()
-    data     = validar.data()
-    telefone = validar.cel()
-    endereço = validar.ed()
-"""
 import os
 from valida import *
 from datetime import datetime
