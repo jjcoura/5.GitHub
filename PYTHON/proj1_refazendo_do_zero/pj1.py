aa82a8f2ed6f71ae1a41e01e46764bc5baf7ce82
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-10 17:12:38 -0300

    a

--- PYTHON/proj1_refazendo_do_zero/pj1.py
+++ PYTHON/proj1_refazendo_do_zero/pj1.py
@@ -1,9 +1,12 @@
 """Programa desenvolvido para cadastro de clientes
 em uma loja..."""
 from defs import *
-from valida import nome
+from valida import *
 
 
 escolha = menu() 
 if escolha == '1':
     cadastro() 
+elif escolha == '2':
+    mostrarDados()
+elif escolha == ''
