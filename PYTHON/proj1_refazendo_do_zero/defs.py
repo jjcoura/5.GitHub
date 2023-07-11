f54698cdfb24f0c56028d9990fab71052846b20d
José Mário Ribeiro Coura <115878025+jjcoura@users.noreply.github.com>
2023-07-11 16:15:35 -0300

    a

--- PYTHON/proj1_refazendo_do_zero/defs.py
+++ PYTHON/proj1_refazendo_do_zero/defs.py
@@ -3,6 +3,26 @@ from datetime import datetime
 from valida import *
 
 def limpaTerminal():
+    """Fução que limpa a tela do terminal
+
+    Returns:
+        os : A função limpaTerminal() é responsável por limpar a tela do terminal/console, removendo qualquer conteúdo anteriormente exibido. Essa função é utilizada para proporcionar uma melhor experiência de uso ao usuário, garantindo que a interface do programa seja limpa e organizada.
+
+A implementação da função limpaTerminal() pode variar dependendo do sistema operacional em que o programa está sendo executado. No código que você forneceu, a função faz uso da biblioteca os para detectar o sistema operacional e executar o comando correspondente para limpar a tela.
+
+import os
+
+def limpaTerminal():
+    return os.system('cls' if os.name == 'nt' else 'clear')
+    
+    
+A função os.name retorna o nome do sistema operacional.
+No caso em que os.name é igual a 'nt', o sistema operacional é Windows, então o comando 'cls' é utilizado para limpar a tela.
+Caso contrário, o sistema operacional é considerado como Unix/Linux, então o comando 'clear' é utilizado para limpar a tela.
+Dessa forma, quando a função limpaTerminal() é chamada, o comando correspondente ao sistema operacional é executado, realizando a limpeza da tela.
+
+É importante ressaltar que a função limpaTerminal() pode não funcionar corretamente em alguns ambientes de desenvolvimento integrados (IDEs) ou em certas situações específicas. Em tais casos, a limpeza da tela pode não ser realizada ou pode produzir resultados indesejados.
+    """
     return os.system('cls' if os.name == 'nt' else 'clear')
 
 def linha():
@@ -14,6 +34,11 @@ mes = data.month
 ano = data.year
 
 def menu():
+    """Menu
+
+    Returns:
+        função menu: Cria um menu personalizado chamado "LOJA LEGAL" há apenas um input para chamar as opções acima para as suas devidas funções serem realizadas.
+    """
     print(' ======== <<< \033[1;36m "LOJA LEGAL" \033[m >>> ====== ')
     print('| \033[1;36m[1]\033[m \033[1;35m    -      Cadastrar Cliente    \033[m|')
     print('| \033[1;36m[2]\033[m \033[1;35m    -      Dados do Cliente     \033[m|')
@@ -26,6 +51,51 @@ def menu():
     return x
 
 def cadastro():
+    """Função CADASTRO:
+    
+    1.  Limpar a tela do terminal:
+* Chama a função limpaTerminal() para limpar a tela antes de exibir as informações do cadastro.
+
+    2.  Exibir mensagem de cabeçalho:
+* Imprime na tela uma mensagem indicando que o usuário está entrando no processo de cadastro.
+
+    3.  Solicitar nome:
+* Chama a função nome() para solicitar e validar o nome do cliente. O nome deve conter apenas letras.
+
+    4.  Solicitar login:
+* Chama a função login() para solicitar e validar o login do cliente. O login deve ter no máximo 12 caracteres e conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um símbolo.
+
+    5.  Verificar se o login já existe:
+* Abre o arquivo logins.txt em modo de leitura e percorre as linhas do arquivo.
+* Verifica se o login fornecido pelo usuário já existe no arquivo.
+* Se o login já existir, exibe uma mensagem de erro e encerra o cadastro.
+* Caso contrário, continua para a próxima etapa.
+
+    6.  Solicitar senha:
+* Chama a função senha() para solicitar e validar a senha do cliente. A senha deve ter exatamente seis dígitos.
+
+    7.  Solicitar e-mail:
+* Chama a função email() para solicitar e validar o e-mail do cliente. O e-mail deve estar em um formato válido.
+
+    8.  Solicitar data de nascimento:
+* Chama a função nascimento() para solicitar e validar a data de nascimento do cliente. A data deve ser uma data válida.
+
+    9.  Solicitar número de telefone:
+* Chama a função cel() para solicitar e validar o número de telefone do cliente. O número de telefone deve conter apenas números e ter entre 9 e 11 dígitos.
+
+    10. Solicitar endereço:
+* Chama a função endereco() para solicitar e validar o endereço completo do cliente. O endereço inclui rua, número, complemento, bairro, cidade, estado, referência e CEP.
+
+    11. Armazenar os dados do cliente:
+* Abre o arquivo logins.txt em modo de escrita (append) para adicionar os dados do cliente ao final do arquivo.
+* Escreve os dados do cliente no formato apropriado, separando cada informação por um hífen (-) e cada campo com seu respectivo valor.
+* Fecha o arquivo após a escrita.
+
+12. Exibir mensagem de sucesso:
+* Exibe uma mensagem indicando que o cliente foi cadastrado com sucesso.
+
+Após executar todas essas etapas, a função cadastro() é concluída e o programa retorna ao menu principal.
+    """
     limpaTerminal()
     print('=== < \033[1;92m        CADASTRAR USUÁRIO\033[m       > ===')
     u_nome = nome()
@@ -54,6 +124,38 @@ def cadastro():
 
 
 def mostrarDados():
+    """Função MOSTRARDADOS:
+
+Aqui está a descrição das etapas dentro da função `mostrarDados()`:
+
+1. Limpar a tela do terminal:
+   - Chama a função `limpaTerminal()` para limpar a tela antes de exibir os dados do cliente.
+
+2. Exibir mensagem de cabeçalho:
+   - Imprime na tela uma mensagem indicando que o usuário precisa fazer login para acessar seus dados.
+
+3. Solicitar login e senha:
+   - Solicita ao usuário que digite seu login e senha.
+   - Armazena o login digitado na variável `userlogin` e a senha digitada na variável `usersenha`.
+
+4. Verificar as informações de login e senha:
+   - Abre o arquivo `logins.txt` em modo de leitura e percorre as linhas do arquivo.
+   - Divide cada linha em valores separados pelo caractere hífen (-).
+   - Verifica se o login e a senha fornecidos pelo usuário correspondem a algum cliente cadastrado.
+   - Se o login e a senha forem encontrados, exibe os dados do cliente na tela.
+   - Caso contrário, exibe uma mensagem de erro informando que o login ou a senha são inválidos.
+
+5. Exibir os dados do cliente:
+   - Se as informações de login e senha forem válidas, imprime na tela os dados do cliente.
+   - Percorre os valores encontrados para o cliente e os exibe um por um, formatados corretamente.
+   - No caso do campo "Endereço", os dados são tratados como um dicionário e são exibidos individualmente.
+
+6. Exibir mensagem de erro:
+   - Se as informações de login e senha forem inválidas ou não correspondem a nenhum cliente cadastrado, exibe uma mensagem de erro informando que o login ou a senha são inválidos.
+
+Após executar essas etapas, a função `mostrarDados()` é concluída e o programa retorna ao menu principal.  
+    
+    """
     limpaTerminal()
     print('=== < \033[1;92m        DADOS DO CLIENTE\033[m       > ===')
     linha()
@@ -105,6 +207,28 @@ def mostrarDados():
     
     
 def senha():
+    """Função SENHA
+
+    Returns:
+       A função `senha()` é responsável por solicitar e validar a senha do cliente durante o processo de cadastro. Ela garante que a senha digitada tenha exatamente seis dígitos.
+
+Aqui está a descrição das etapas dentro da função `senha()`:
+
+1. Exibir mensagem de orientação:
+   - Imprime na tela uma mensagem orientando o usuário a digitar uma senha com seis dígitos.
+
+2. Solicitar senha:
+   - Chama a função `input()` para solicitar ao usuário que digite a senha.
+   - Armazena a senha digitada na variável `senha`.
+
+3. Validar a senha:
+   - Verifica se a senha possui exatamente seis dígitos.
+   - Se a senha não tiver seis dígitos, exibe uma mensagem de erro e retorna ao passo 2 para solicitar uma nova senha.
+   - Caso contrário, a senha é considerada válida e a função é concluída, retornando a senha digitada.
+
+Essa é a descrição das etapas dentro da função `senha()`. Ela garante que a senha tenha o formato esperado, exigindo que tenha exatamente seis dígitos. Caso contrário, solicita ao usuário que insira uma nova senha até que seja fornecida uma senha válida.
+
+    """
     while True:
         print('Digite uma senha com seis dígitos!')
         print('Conselho: "evite datas de aniversário ou senhas sequenciais".')
@@ -118,6 +242,36 @@ def senha():
         
 
 def clientesCadastrados():
+    """Função clientesCadastrados:
+    
+    A função `clientesCadastrados()` é responsável por exibir na tela todos os clientes cadastrados no arquivo `logins.txt`. Ela percorre o arquivo, lê as linhas contendo as informações dos clientes e imprime cada linha na saída.
+
+Resumo da função `clientesCadastrados()`:
+
+1. Limpar a tela do terminal:
+   - Chama a função `limpaTerminal()` para limpar a tela antes de exibir os clientes cadastrados.
+
+2. Exibir mensagem de cabeçalho:
+   - Imprime na tela uma mensagem indicando que a lista de clientes cadastrados será exibida.
+
+3. Abrir o arquivo `logins.txt`:
+   - Abre o arquivo `logins.txt` em modo de leitura.
+
+4. Percorrer as linhas do arquivo:
+   - Utiliza um loop para percorrer cada linha do arquivo.
+
+5. Exibir cada linha na tela:
+   - Imprime cada linha lida do arquivo, que contém as informações de um cliente cadastrado.
+   - Utiliza a função `print()` para exibir a linha na saída.
+
+6. Fechar o arquivo:
+   - Fecha o arquivo `logins.txt` após a leitura.
+
+Após executar essas etapas, a função `clientesCadastrados()` é concluída e o programa retorna ao menu principal.
+
+Essa é a descrição das etapas dentro da função `clientesCadastrados()`. Ela percorre o arquivo `logins.txt` e exibe na tela todas as linhas contendo as informações dos clientes cadastrados.
+
+    """
     limpaTerminal()
     print('=== < \033[1;92m        CLIENTES CADASTRADOS\033[m       > ===')
     logins = open('logins.txt', 'r')
@@ -129,6 +283,50 @@ def clientesCadastrados():
 
 
 def relatorio():
+    """Função relarotio:
+    
+    Descrição do que cada item da função `relatorio()`:
+
+1. Contagem de clientes cadastrados:
+   - Inicializa a variável `cc` como zero para contar o número de clientes cadastrados.
+   - Inicializa a lista `nomess` para armazenar os nomes dos clientes.
+
+2. Leitura do arquivo `logins.txt`:
+   - Abre o arquivo `logins.txt` em modo de leitura.
+
+3. Contagem e armazenamento dos nomes dos clientes:
+   - Percorre cada linha do arquivo `logins.txt` utilizando um loop `for`.
+   - Divide cada linha em valores separados pelo caractere hífen (-).
+   - Extrai o nome do cliente da primeira informação da linha.
+   - Adiciona o nome à lista `nomess`.
+   - Incrementa a variável `cc` para contar o número de clientes cadastrados.
+
+4. Fechamento do arquivo `logins.txt`:
+   - Fecha o arquivo `logins.txt` após a leitura.
+
+5. Limpeza da tela:
+   - Chama a função `limpaTerminal()` para limpar a tela antes de exibir o relatório.
+
+6. Criação do diretório `RELATORIO_proj1`:
+   - Verifica se o diretório `RELATORIO_proj1` já existe.
+   - Se não existir, cria o diretório utilizando a função `os.makedirs()`.
+
+7. Criação do arquivo `dados.txt`:
+   - Define o caminho do arquivo `dados.txt` no diretório `RELATORIO_proj1` utilizando a função `os.path.join()`.
+   - Abre o arquivo `dados.txt` em modo de escrita (`w+`).
+   - Escreve a linha de título "Relatório de Clientes" no arquivo.
+   - Escreve uma linha em branco.
+   - Escreve a quantidade de clientes cadastrados no arquivo.
+   - Escreve uma linha em branco.
+   - Percorre a lista `nomess` e escreve cada nome de cliente no arquivo, numerando-os.
+   - Escreve a linha com a data atual no formato "Brasil, dia/mês/ano".
+   - Fecha o arquivo `dados.txt`.
+
+8. Exibição de mensagem de sucesso:
+   - Imprime uma linha na tela indicando que o relatório foi gerado com sucesso.
+
+Após a conclusão dessas etapas, a função `relatorio()` é finalizada e o programa retorna ao menu principal.
+    """
     cc = 0
     nomess = []
     logins = open('logins.txt', 'r')
@@ -160,6 +358,27 @@ def relatorio():
 
 
 def verifica_arquivo_logins():
+    """função verifica_arquivo_logins:
+    
+    A função `verifica_arquivo_logins()` é responsável por verificar se o arquivo `logins.txt` existe no diretório atual. Caso o arquivo não exista, a função cria um novo arquivo vazio.
+
+Aqui está a descrição das etapas dentro da função `verifica_arquivo_logins()`:
+
+1. Verificar a existência do arquivo `logins.txt`:
+   - Utiliza a função `os.path.exists()` para verificar se o arquivo `logins.txt` existe no diretório atual.
+
+2. Criação do arquivo `logins.txt`:
+   - Caso o arquivo `logins.txt` não exista, é aberto o arquivo em modo de escrita (`'w'`).
+   - Como o arquivo ainda não existe, ele é criado vazio.
+
+3. Fechamento do arquivo:
+   - Fecha o arquivo `logins.txt` após a verificação ou criação.
+
+Após executar essas etapas, a função `verifica_arquivo_logins()` é concluída.
+
+Essa é a descrição das etapas dentro da função `verifica_arquivo_logins()`. Ela garante a existência do arquivo `logins.txt` no diretório atual, criando-o caso não exista. Isso é útil para garantir que o programa tenha o arquivo necessário para armazenar os dados dos clientes.
+
+    """
     if not os.path.exists('logins.txt'):
         with open('logins.txt', 'w') as arquivo:
             arquivo.write('')
--- PYTHON/proj1_refazendo_do_zero/valida.py
+++ PYTHON/proj1_refazendo_do_zero/valida.py
@@ -2,6 +2,26 @@ from defs import *
 from datetime import date, datetime
 
 def nome():
+    """função nome:    
+
+    Returns:
+        A função `nome()`:
+
+A função `nome()` é responsável por solicitar e validar o nome do cliente durante o processo de cadastro. Ela garante que o nome fornecido contenha apenas letras, sem números ou caracteres especiais.
+
+Aqui está a descrição das etapas dentro da função `nome()`:
+
+1. Solicitar nome:
+   - Utiliza a função `input()` para solicitar ao usuário que digite o nome.
+
+2. Validar o nome:
+   - Verifica se o nome fornecido contém apenas letras, sem números ou caracteres especiais.
+   - Se o nome não estiver de acordo com o critério, exibe uma mensagem de erro e retorna ao passo 1 para solicitar um novo nome.
+   - Caso contrário, o nome é considerado válido e a função é concluída, retornando o nome digitado.
+
+Essa é a descrição das etapas dentro da função `nome()`. Ela garante que o nome fornecido seja válido, consistindo apenas de letras.
+
+    """
     while True:
         nome = input('Nome: ')
         if nome == '':
@@ -14,6 +34,29 @@ def nome():
         
         
 def senha():
+    """função senha: 
+    
+    Descrevendo da função `senha()`:
+
+A função `senha()` é responsável por solicitar e validar a senha do cliente durante o processo de cadastro. Ela garante que a senha digitada tenha exatamente seis dígitos.
+
+Aqui está a descrição das etapas dentro da função `senha()`:
+
+1. Exibir mensagem de orientação:
+   - Imprime na tela uma mensagem orientando o usuário a digitar uma senha com seis dígitos.
+
+2. Solicitar senha:
+   - Utiliza a função `input()` para solicitar ao usuário que digite a senha.
+   - Armazena a senha digitada na variável `senha`.
+
+3. Validar a senha:
+   - Verifica se a senha possui exatamente seis dígitos.
+   - Se a senha não tiver seis dígitos, exibe uma mensagem de erro e retorna ao passo 2 para solicitar uma nova senha.
+   - Caso contrário, a senha é considerada válida e a função é concluída, retornando a senha digitada.
+
+Essa é a descrição das etapas dentro da função `senha()`. Ela garante que a senha tenha o formato esperado, exigindo que tenha exatamente seis dígitos. Caso contrário, solicita ao usuário que insira uma nova senha até que seja fornecida uma senha válida.
+
+    """
     while True:
         print('Digite uma senha com seis dígitos!')
         print('Conselho: "evite datas de aniversário ou senhas sequenciais".')
@@ -27,6 +70,26 @@ def senha():
         
     
 def email():
+    """função email:
+
+    Returns:
+        Função `email()`:
+
+A função `email()` é responsável por solicitar e validar o e-mail do cliente durante o processo de cadastro. Ela verifica se o e-mail fornecido possui um formato válido, contendo o caractere "@" e o domínio ".com".
+
+Aqui está a descrição das etapas dentro da função `email()`:
+
+1. Solicitar e-mail:
+   - Utiliza a função `input()` para solicitar ao usuário que digite o e-mail.
+
+2. Validar o e-mail:
+   - Verifica se o e-mail contém o caractere "@" e o domínio ".com".
+   - Se o e-mail não estiver no formato válido, exibe uma mensagem de erro e retorna ao passo 1 para solicitar um novo e-mail.
+   - Caso contrário, o e-mail é considerado válido e a função é concluída, retornando o e-mail digitado.
+
+Essa é a descrição das etapas dentro da função `email()`. Ela garante que o e-mail fornecido seja válido, verificando se possui o formato esperado com o caractere "@" e o domínio ".com".
+
+    """
     while True:
         print('Por favor digite seu e-mail.')
         email = input('Email: ')
@@ -39,6 +102,49 @@ def email():
             
             
 def validar_nascimento(nascimento):
+    """função validar_nascimento(nascimento):
+
+    Args:
+        nascimento (_type_list): _description_ 
+        A função `nascimento()` é uma função que retorna uma lista de valores. 
+
+A função `nascimento()` solicita ao usuário que digite o dia, mês e ano de nascimento e retorna esses valores em uma lista. A lista contém três elementos: o dia, o mês e o ano fornecidos pelo usuário.
+
+Exemplo de uso da função `nascimento()`:
+
+```python
+data_nascimento = nascimento()
+print(data_nascimento)  # Saída: [10, 5, 1990]
+```
+
+No exemplo acima, a função `nascimento()` é chamada e o usuário digita o dia 10, o mês 5 e o ano 1990. A função retorna a lista `[10, 5, 1990]`, que pode ser atribuída a uma variável para posterior uso.
+
+Portanto, o tipo de retorno da função `nascimento()` é `list`.
+
+    Returns:
+    
+        Descrevando da função `validar_nascimento(nascimento)`:
+
+A função `validar_nascimento(nascimento)` é responsável por verificar se uma data de nascimento fornecida é válida. Ela recebe como entrada uma data no formato de string e realiza uma série de verificações para garantir que a data seja uma data válida, considerando os meses com 30 ou 31 dias e o mês de fevereiro em anos bissextos.
+
+Aqui está a descrição das etapas dentro da função `validar_nascimento(nascimento)`:
+
+1. Verificar o formato da data:
+   - Utiliza a função `strptime()` do módulo `datetime` para converter a string de data fornecida em um objeto `datetime`.
+   - Caso ocorra uma exceção do tipo `ValueError`, retorna `False` indicando que a data é inválida.
+
+2. Validar o dia e o mês:
+   - Obtém o dia, mês e ano a partir do objeto `datetime`.
+   - Verifica se o mês possui 30 ou 31 dias, considerando os meses 4, 6, 9 e 11.
+   - Verifica se o mês é fevereiro (valor 2) e, se for, verifica se o ano é bissexto.
+   - Retorna `False` se o dia ou o mês forem inválidos.
+
+3. Retornar o resultado:
+   - Se todas as verificações passarem com sucesso, retorna `True` indicando que a data de nascimento é válida.
+
+Essa é a descrição das etapas dentro da função `validar_nascimento(nascimento)`. Ela realiza a validação de uma data de nascimento, verificando se a data fornecida é válida de acordo com as regras de calendário.
+
+    """
     try:
         formato = "%d/%m/%Y"
         data_formatada = datetime.strptime(nascimento, formato)
@@ -63,6 +169,28 @@ def validar_nascimento(nascimento):
 
 
 def nascimento():
+    """função nascimento:
+
+    Returns:
+        A função `def nascimento():` está sendo utilizada no programa para solicitar ao usuário que digite o dia, mês e ano de nascimento. Ela é responsável por validar e retornar os valores fornecidos pelo usuário em uma lista.
+
+1. Solicitação de dados de nascimento:
+   - A função `nascimento()` é chamada em determinado ponto do programa, onde se deseja obter a data de nascimento do cliente.
+   - Ela exibe mensagens solicitando ao usuário que digite o dia, mês e ano de nascimento.
+
+2. Validação dos dados de nascimento:
+   - A função verifica se os valores fornecidos pelo usuário são numéricos, utilizando a função `isdigit()`.
+   - Verifica se o dia, mês e ano são válidos, de acordo com as regras de calendário.
+   - Se algum valor não for válido, a função exibe uma mensagem de erro e solicita que o usuário insira novamente os dados corretos.
+
+3. Retorno dos dados de nascimento:
+   - Após a validação dos valores fornecidos pelo usuário, a função retorna os dados em uma lista, onde o primeiro elemento é o dia, o segundo é o mês e o terceiro é o ano.
+   - Essa lista pode ser armazenada em uma variável para ser utilizada posteriormente no programa.
+
+O comportamento da função `def nascimento():` é de solicitar e validar os dados de nascimento do usuário, garantindo que sejam valores numéricos e obedecendo às regras de calendário. Ela retorna esses valores em uma lista para serem utilizados no restante do programa.
+
+
+    """
     while True:
         dia = input('Digite o dia de nascimento: ')
         mes = input('Digite o mês de nascimento: ')
@@ -102,6 +230,32 @@ def nascimento():
 
 
 def login():
+    """função login:
+
+    Returns:       
+
+A função `login()` é responsável por solicitar e validar o login do cliente durante o processo de cadastro. Ela garante que o login atenda a determinados critérios, como ter no máximo 12 caracteres, conter letras maiúsculas, letras minúsculas, números e símbolos.
+
+
+1. Exibir mensagem de orientação:
+   - Imprime na tela uma mensagem orientando o usuário a digitar um login válido.
+
+2. Solicitar login:
+   - Utiliza a função `input()` para solicitar ao usuário que digite o login.
+   - Armazena o login digitado na variável `login`.
+
+3. Validar o login:
+   - Verifica se o login tem no máximo 12 caracteres, utilizando a função `len()`.
+   - Verifica se o login contém pelo menos uma letra maiúscula, utilizando a função `isupper()`.
+   - Verifica se o login contém pelo menos uma letra minúscula, utilizando a função `islower()`.
+   - Verifica se o login contém pelo menos um número, utilizando a função `isdigit()`.
+   - Verifica se o login contém pelo menos um caractere não alfanumérico, utilizando a função `isalnum()`.
+   - Se o login não atender a algum dos critérios acima, exibe uma mensagem de erro e retorna ao passo 2 para solicitar um novo login.
+   - Caso contrário, o login é considerado válido e a função é concluída, retornando o login digitado.
+
+Essa é a descrição das etapas dentro da função `login()`. Ela garante que o login fornecido atenda aos critérios estabelecidos, incluindo comprimento máximo, presença de letras maiúsculas, minúsculas, números e caracteres especiais. Caso o login não atenda a esses critérios, solicita ao usuário que insira um novo login até que seja fornecido um login válido.
+
+    """
     while True:
         print('Seu login deve conter no máximo 12 caracteres com letras maiúsculas, minúsculas, símbolos e números.')
         login = input('Digite seu login: ')
@@ -123,6 +277,27 @@ def login():
     
     
 def cel():
+    """a função cel de telefone celular:
+
+    Returns:
+        
+
+A função `cel()` é responsável por solicitar e validar o número de telefone celular do cliente durante o processo de cadastro. Ela verifica se o número fornecido contém apenas dígitos numéricos e se está dentro de um intervalo válido de comprimento.
+
+Descrição:
+
+1. Solicitar número de telefone:
+   - Utiliza a função `input()` para solicitar ao usuário que digite o número de telefone celular.
+
+2. Validar o número de telefone:
+   - Verifica se o número fornecido contém apenas dígitos numéricos, utilizando a função `isdigit()`.
+   - Verifica se o número de telefone possui um comprimento válido.
+   - Se o número de telefone não atender aos critérios acima, exibe uma mensagem de erro e retorna ao passo 1 para solicitar um novo número de telefone.
+   - Caso contrário, o número de telefone é considerado válido e a função é concluída, retornando o número digitado.
+
+Essa é a descrição das etapas dentro da função `cel()`. Ela garante que o número de telefone celular fornecido seja válido, verificando se contém apenas dígitos numéricos e se possui um comprimento dentro de um intervalo válido.
+
+    """
     while True:
         cel = input('Digite seu número de telefone: ')
         if cel == '':
@@ -136,6 +311,33 @@ def cel():
         
     
 def endereco():
+    """função endereco:
+
+    Returns:
+        Descrevendo a função `endereco()`:
+
+A função `endereco()` é responsável por solicitar e validar o endereço completo do cliente durante o processo de cadastro. Ela solicita informações como rua, número, complemento, bairro, cidade, estado e ponto de referência, e retorna essas informações em um dicionário.
+
+Etapas:
+
+1. Solicitar informações do endereço:
+   - Utiliza a função `input()` para solicitar ao usuário que digite as informações do endereço, como rua, número, complemento, bairro, cidade, estado e ponto de referência.
+
+2. Armazenar informações do endereço em um dicionário:
+   - Cria um dicionário vazio chamado `dados` para armazenar as informações do endereço.
+   - Utiliza a função `input()` para solicitar cada informação do endereço individualmente e armazena o valor fornecido pelo usuário no dicionário `dados`, utilizando as chaves correspondentes.
+
+3. Solicitar o CEP (opcional):
+   - Utiliza a função `input()` para solicitar ao usuário que digite o CEP do endereço.
+   - Verifica se o CEP possui o comprimento correto (8 dígitos) e se contém apenas dígitos numéricos.
+   - Caso o CEP não atenda aos critérios acima, o valor é armazenado como uma string vazia no dicionário `dados`.
+
+4. Retornar o dicionário com as informações do endereço:
+   - Após solicitar e armazenar todas as informações do endereço, a função retorna o dicionário `dados`, que contém as informações completas do endereço.
+
+Essa é a descrição das etapas dentro da função `endereco()`. Ela solicita e valida as informações completas do endereço do cliente, armazenando essas informações em um dicionário. O dicionário é então retornado pela função, permitindo que as informações do endereço sejam utilizadas posteriormente no programa.
+
+    """
     while True:
         print('=========\033[1;32mSeu endereço completo!\033[m ========') 
         dados = {'Rua': input('Rua: '),
